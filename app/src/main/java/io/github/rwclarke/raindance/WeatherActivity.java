2c399a9e5780a55ca071d9aea5a98c5a30bc817f
Ryan Clarke <ryan.w.clarke@outlook.com>
2017-02-16 10:23:41 -0500

    added premissions, google location services, Interface Delegate Methods

--- app/src/main/java/io/github/rwclarke/raindance/WeatherActivity.java
+++ app/src/main/java/io/github/rwclarke/raindance/WeatherActivity.java
@@ -1,5 +1,8 @@
 package io.github.rwclarke.raindance;
 
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,22 +13,37 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONObject;
 
-public class WeatherActivity extends AppCompatActivity {
+public class WeatherActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
 
     final String URL_BASE = "http://api.openweathermap.org/data/2.5/forecast";
     final String URL_COORD = "?lat=45.3639363&lon=-75.6420132" ;
     final String API_KEY = "&APPID=62782d3f768215474f42e7b8dc452d18";
     final String URL_UNIT = "&units=metric";
-    //45.3639363,-75.6420132
+
+    private GoogleApiClient mGoogleApiClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .enableAutoManage(this, this) // managed onStart() and onStop()
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    public void downloadWeatherData() {
         final String url = URL_BASE + URL_COORD + URL_UNIT + API_KEY;
 
         final JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>(){
@@ -41,5 +59,27 @@ public class WeatherActivity extends AppCompatActivity {
         });
 
         Volley.newRequestQueue(this).add(jsonRequest);
+    }
+
+    // Interface Delegate Methods
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
     }
 }
